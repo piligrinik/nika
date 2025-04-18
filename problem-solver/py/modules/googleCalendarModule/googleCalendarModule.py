d9e38d438946a1601b6a8e86890f73c6a99b165a
Alexey Kharietskiy <slashertoor@gmail.com>
2025-04-18 16:04:26 +0300

    feat(solver): init google calendar module

---
+++ problem-solver/py/modules/googleCalendarModule/googleCalendarModule.py
@@ -0,0 +1,5 @@
+from sc_kpm import ScModule
+
+
+class GoogleCalendarModule(ScModule):
+    ...
--- problem-solver/py/server.py
+++ problem-solver/py/server.py
@@ -1,6 +1,7 @@
 import argparse
 from sc_kpm import ScServer
 from modules.messageProcessingModule.MessageProcessingModule import MessageProcessingModule
+from modules.googleCalendarModule.googleCalendarModule import GoogleCalendarModule
 
 from pathlib import Path
 
@@ -19,7 +20,8 @@ def main(args: dict):
 
     with server.connect():
         modules = [
-            MessageProcessingModule()
+            MessageProcessingModule(),
+            GoogleCalendarModule()
         ]
         server.add_modules(*modules)
         with server.register_modules():
