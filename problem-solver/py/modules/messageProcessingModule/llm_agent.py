1079bf1f6b4847df3d7a56fa2331bd6d1ec9cc1f
piligrinik <grinevskayam@gmail.com>
2025-11-26 16:56:24 +0300

    feat: llm_agent logic + gwf

--- problem-solver/py/modules/messageProcessingModule/llm_agent.py
+++ problem-solver/py/modules/messageProcessingModule/llm_agent.py
@@ -8,14 +8,6 @@ from sc_kpm.identifiers import CommonIdentifiers
 from sc_kpm import ScAgentClassic, ScModule, ScResult, ScServer
 from sc_kpm.sc_sets import ScSet
 from sc_kpm.utils import (
-    create_link,
-    get_link_content_data,
-    check_edge, create_edge,
-    delete_edges,
-    get_element_by_role_relation,
-    get_element_by_norole_relation,
-    get_system_idtf,
-    get_edge,
     generate_link,
     generate_links,
     generate_non_role_relation,
@@ -24,8 +16,7 @@ from sc_kpm.utils import (
 from sc_kpm.utils.action_utils import (
     generate_action_result,
     finish_action_with_status,
-    get_action_arguments,
-    search_element_by_role_relation,
+    get_action_arguments
     
 )
 from sc_kpm import ScKeynodes
@@ -59,12 +50,12 @@ class LLMAgent(ScAgentClassic):
         result = self.run(action_element)
         is_successful = result == ScResult.OK
         finish_action_with_status(action_element, is_successful)
-        self.logger.info("WeatherAgent finished %s",
+        self.logger.info("LLMAgent finished %s",
                          "successfully" if is_successful else "unsuccessfully")
         return result
     
     def run(self, action_node: ScAddr) -> ScResult:
-        self.logger.info("WeatherAgent started")
+        self.logger.info("LLMAgent started")
         try:
             message_addr = get_action_arguments(action_node, 1)[0]
             
@@ -76,8 +67,7 @@ class LLMAgent(ScAgentClassic):
             #         f"WeatherAgent: the message isn’t about weather")
             #     return ScResult.OK
 
-            rrel_entity = ScKeynodes.resolve("rrel_entity", sc_type.NODE_ROLE)
-            entity_addr = ScAddr(0) # TODO: take Alice's code here (search for entity address)
+            entity_node = ScAddr(0) # TODO: take Alice's code here (search for entity address)
             # at this point i will have addresses: entity
 
             json_input = {
@@ -88,48 +78,41 @@ class LLMAgent(ScAgentClassic):
                 "definition": None,
                 "answer": None
                 }
-            json_about_entity = {
-                "title": "Question about an entity",
-                "description": "A question about an entity, its name, definition and the answer in human format.",
-                "type": "object",
-                "properties": {
-                    "question": {
-                        "type": "string",
-                        "description": "The question about the definition of the entity, will always be mentioned in human question."
-                    },
-                    "entity": {
-                        "type": "string",
-                        "description": "Entity that the question is about."
-                    },
-                    "question_type": {
-                        "type": "string",
-                        "description": "Type of the question. Ex: about_entity, about_weather."
-                    },
-                    "nrel_main_idtf": {
-                        "type": "string",
-                        "description": "Identifier of the entity in the initial form (with a capital letter)."
-                    },
-                    "nrel_def": {
-                        "type": "string",
-                        "description": "A definition of the entity without mentioning the entity itself."
-                    },
-                    "answer": {
-                        "type": "string",
-                        "description": "An answer to human question in a natural language."
-                    }
-                },
-                "required": ["question", "entity", "nrel_main_idtf", "definition", "answer"]
-            }
-            json_output = self.get_llm_answer(json_template=json_about_entity, json_input=json_input)
+            llm_template = ScAddr(0)
+            
+            rrel_prompt_json = ScKeynodes.resolve("rrel_prompt_json", None)
+            self.logger.debug(f"rrel_prompt_json valid: {rrel_prompt_json.is_valid()}")
+            llm_template_args = ScTemplate()
+            llm_template_args.triple(
+                llm_template,
+                sc_type.VAR_TEMP_POS_ARC,
+                sc_type.VAR_NODE_TUPLE >> "_param_tuple"
+            )
+            llm_template_args.quintuple(
+                "_param_tuple",
+                sc_type.VAR_TEMP_POS_ARC,
+                sc_type.VAR_NODE_LINK >> "_json_template",
+                sc_type.VAR_TEMP_POS_ARC,
+                rrel_prompt_json
+            )
+            llm_templ_result = search_by_template(llm_template_args)
+            json_template = llm_templ_result.get("_json_template")
+            self.logger.debug(f"Json template found: {json_template.is_valid()}")        
+
+            json_output = self.get_llm_answer(json_template=json_template, json_input=json_input)
             diff_params = self.find_new_params(json_input, json_output)
-        except:
-            self.logger.info(f"LLMAgent: finished with an error")
+            self.add_new_params_to_kb(diff_params, entity_node)
+            answer_txt = json_output["answer"]
+            reply_node = self.add_answer_to_result(message_addr, answer_txt)
+            generate_action_result(action_node, reply_node)
+            ScResult.OK
+        except Exception as e:
+            self.logger.info(f"LLMAgent: finished with an error: {e}")
             return ScResult.ERROR
         
-    def get_llm_answer(self, json_template: dict, json_input: dict):
-# TODO: add parameters 
-# TODO: adopt dynamic json
-# TODO: llm_json_template_to_kb
+    def get_llm_answer(self, json_template: dict, json_input: dict): 
+
+# TODO: работа с json!!!
 
         llm = init_chat_model("google_genai:gemini-2.5-flash-lite")
 
@@ -138,34 +121,24 @@ class LLMAgent(ScAgentClassic):
             method="json_schema",
         )
         
-        response = model_with_structure.invoke(f"""Please complite provided JSON, don't change the fields that are not None: \n 
-                # {json_input}""")
+        response = json.loads(model_with_structure.invoke(f"""Please complite provided JSON, don't change the fields that are not None: \n 
+                 {json_input}"""))
         return response
     
     def find_new_params(self, json_input: dict, json_output: dict):
-        # json_output = json.dumps(response, ensure_ascii=False, indent=2)
-        # json_output = response
-        json_output = {
-        "question": "Что такое апельсин?",
-        "entity": "orange",
-        "nrel_main_idtf": "Апельсин",
-        "definition": "Плод апельсинового дерева.",
-        "answer": "Апельсин — это плод апельсинового дерева.",
-        "question_type": "about_entity"
-        }
         diff = {}
         if json_output.keys() == json_input.keys():
             for v1, v2 in zip(sorted(json_input.items()), sorted(json_output.items())):
                 if v1 != v2:
-                    print(f"Different field: {v1} and {v2}")
+                    self.logger.debug(f"Different field: {v1} and {v2}")
                     diff[v1[0]] = v2[1]
         return diff
     
-    def add_new_params_to_kb(self, new_params: dict, entity_addr: ScAddr):
-
-        new_links = generate_links(new_params.values())
+    def add_new_params_to_kb(self, new_params: dict, entity_node: ScAddr):
+        new_links = generate_links(new_params.values(), ScLinkContentType.STRING, sc_type.CONST_NODE_LINK)
         for link, key in zip(new_links, new_params.keys()):
-            nrel = generate_non_role_relation(entity_addr, link, generate_node(sc_type.CONST_NODE_NON_ROLE, key))
+            nrel = generate_non_role_relation(entity_node, link, generate_node(sc_type.CONST_NODE_NON_ROLE, key))
+        self.logger.info(f"New params are linked to an entity")
 
     def add_answer_to_result(self, message_addr: ScAddr, answer_text: str):
         # generate answer link
@@ -173,7 +146,7 @@ class LLMAgent(ScAgentClassic):
         answer_link_node = generate_node(sc_type.NODE)
         # resolve or create all necessary nodes 
         nrel_reply_node = ScKeynodes.resolve("nrel_reply", None)
-        text_translation_node = ScKeynodes.resolve("nrel_sc_text_translation", sc_type.CONST_NODE_NON_ROLE)
+        text_translation_node = ScKeynodes.resolve("nrel_sc_text_translation", None)
         reply_message_node = ScKeynodes.resolve("reply_message", sc_type.NODE)
         
         answer_template = ScTemplate()
@@ -203,10 +176,9 @@ class LLMAgent(ScAgentClassic):
                   "_reply_node": reply_message_node,
                   "_nrel_reply": nrel_reply_node,
                   "_nrel_sc_text_translation": text_translation_node}
-        search_results = generate_by_template(answer_template, params)
-        
-
-
+        results = generate_by_template(answer_template, params)
+        self.logger.info(f"Result message is formed: {results}")
+        return reply_message_node
 
         
 
